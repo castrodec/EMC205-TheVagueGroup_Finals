3fd7ca1fe808dcc35bc76b413e7add6e2e52b87d
castrodec <meshelemiah.castro@ciit.edu.ph>
2026-03-22 15:32:36 +0800

    (visuals) Added new mouse cursor, changed Main Menu Layout, New game icon

--- Assets/MainMenu/MainMenuManager.cs
+++ Assets/MainMenu/MainMenuManager.cs
@@ -5,11 +5,11 @@ public class MainMenuManager : MonoBehaviour
 {
     //Setup
     //Step 1: Place this script on the parent Canvas.
-    //Setp 2: Create a button and place it as a child of the Canvas.
+    //Step 2: Create a button and place it as a child of the Canvas.
     //Step 3: In the button's OnClick() event, drag the Canvas into the object field and select MainMenuManager -> LoadOnClick(Object) as the function.
     //Step 4: In the button's OnClick() event, drag the scene you want to load into the object field.
-    
-        public void LoadOnClick(Object sceneToLoad)
+
+    public void LoadOnClick(Object sceneToLoad)
     {
         if (sceneToLoad != null)
             SceneManager.LoadScene(sceneToLoad.name);
